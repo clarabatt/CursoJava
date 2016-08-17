654e09c076f35f59abe8b3f1bbdd5ebf08650c1b
Clara Battesini <clarabattesini@gmail.com>
2016-08-17 17:05:52 -0300

    primeira aula

---
+++ Anotações/Aula01.java
@@ -0,0 +1,31 @@
+// Primeiro Programa
+// O Java segue o padrão CamelCase, que é todas as letras iniciais de cada palavra maiúsculas.
+// Nomes de Classes e de arquivos seguem esse padrão.
+// Java é case sensitive!
+
+// Compilar
+// javac NomeDoArquivo.java
+/* Para salvar os arquivos do projeto, você deve salvá-los como .java, 
+depois compilando será criado um arquivo .class com o mesmo nome da classe 
+criada no arquivo .java, por isso os nomes, do arquivo e da classe, devem ser iguais. */
+
+// Executar
+// java NomeDoArquivo
+// Este é o comando usado para executar o código. Não é necessário colocar a extensão .class 
+
+// Passo a Passo
+// declarando uma classe com "class"
+class Aula01{   // Nome da classe é Aula01
+    public static void main(String[] args){     // Ponto de Entrada do Java (Método)
+        System.out.println("Hello World!");     // Comando de saída de impressão
+    }
+}
+
+class NomeDaClasse{   // Nome da classe é Aula01
+    public static void NomeDoMetodo(parametros){     // Ponto de Entrada do Java (Método)
+        // Código Aqui!
+    }
+}
+// public static é um modificador de acesso dos métodos
+// void é o tipo de retorno do método, nesse caso não retorna nada
+// Blocos estão entre chaves, cada abertura de chaves é um bloco, nesse caso, temos 2 blocos.
